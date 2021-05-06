b55e5e2fe070a2c62bce1a37275943814a7eb30e
HiAIDev <asahide0026@gmail.com>
2021-05-05 22:15:35 -0300

    User model and relations

--- ESHIRT/api/src/db.js
+++ ESHIRT/api/src/db.js
@@ -30,10 +30,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Shirts } = sequelize.models;
+const { Shirts, Users } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Users.belongToMany(Shirts, {through: 'favorites'})
+Users.hasMany(Order)
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
