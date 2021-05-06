795a706d0ee581b7526cbbe22f1652d16eaf3f05
HiAIDev <asahide0026@gmail.com>
2021-05-06 15:28:45 -0300

    Routes created

--- ESHIRT/api/src/routes/index.js
+++ ESHIRT/api/src/routes/index.js
@@ -2,12 +2,15 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
-
-const router = Router();
+const {category, details, order, shirt, user}= require('./')
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use('/category', category)
+router.use('/details', details)
+router.use('/order', order)
+router.use('/shirt', shirt)
+router.use('/user', user)
 
 module.exports = router;
