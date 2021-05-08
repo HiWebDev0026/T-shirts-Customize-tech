bf2ac8821397af70f44646a3fd795fdb6bf0248a
HiAIDev <asahide0026@gmail.com>
2021-05-07 19:38:01 -0500

    1era version

--- ESHIRT/client/src/App.js
+++ ESHIRT/client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './Components/Home/Home.jsx';
+import Cart from './Components/Cart/Cart.jsx';
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Shirts</h1>
+      <Home/>
     </div>
   );
 }
---
+++ ESHIRT/client/src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from '../Carousel/Carousel.jsx';
+
+export default function Home (){
+    return(
+        <div>
+            <h1>Here goes the Nav Bar and Search Bar</h1>
+            <Carousel/>
+            <h1>Here goes the Foother</h1>
+        </div>
+    )
+};
