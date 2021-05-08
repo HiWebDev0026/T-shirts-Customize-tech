a9c17cdfca7ea7a32b7a7b6cb3cc7d1f98012d22
HiAIDev <asahide0026@gmail.com>
2021-05-08 15:39:50 -0300

    paginate and catalogue

--- ESHIRT/client/src/App.js
+++ ESHIRT/client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './Components/Home/Home.jsx'
 import NavBar from './Components/NavBar/NavBar.jsx';
-import Cards from './Components/Catalogue/Cards/Cards.jsx';
+import Catalogue from './Components/Catalogue/Catalogue.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 import Design from './Components/Designer/Design.jsx';
 import {Route} from 'react-router-dom';
@@ -11,7 +11,7 @@ function App() {
     <div className= 'App'>
       <Route path= '/' component={NavBar}/>
       <Route exact path= '/' component={Home}/>
-      <Route exact path= '/catalogue' component={Cards}/>
+      <Route exact path= '/catalogue' component={Catalogue}/>
       <Route exact path= '/design' component={Design}/>
       <Route path= '/' component={Footer}/>
     </div>
