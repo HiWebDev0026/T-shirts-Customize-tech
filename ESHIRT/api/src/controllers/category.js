dfacd443da977bec61c52c3c0b9bd4517fb5c456
HiAIDev <asahide0026@gmail.com>
2021-05-10 20:37:41 -0300

    validations updated

--- ESHIRT/api/src/controllers/category.js
+++ ESHIRT/api/src/controllers/category.js
@@ -5,17 +5,39 @@ const {Category, Shirt} = require('../db.js');
 // renombrar los archivos controller.js cuando estén las rutas definidas
 
 // COMENTARIO
+
+
+const filterInvalidCharts = (txt) => {
+    let result = '';
+    for (let i=0; i < txt.length; i++) {
+        let ch = txt[i];
+        if (ch.toUpperCase() !== ch.toLowerCase()) {result += ch}
+    }
+    return result;
+}
+
+
+const validateBody = (body) => {
+    if (!body.hasOwnProperty('name')) return false;
+    if (!body.name) return false;
+    if (!typeof body.name === 'string') return false;
+    if (!isNaN(body.name)) return false;
+
+    return filterInvalidCharts(body.name)
+}
     
+
 async function postCategory(req, res, next) {
-    
-      
     try {
-        
-        const newCategory= {...req.body, name: req.body.name.toLowerCase()}
+        const name = validateBody(req.body)
+
+        if (!name) { return next({status: 400, message: 'Bad body request'})};
+
+        const newCategory= {...req.body, name: name.toLowerCase()}
         const postedCategory = await Category.create(newCategory);
         return res.status(200).json(postedCategory)
     } catch (error) {
-        next({status: 409, message: 'Category already exist'});
+        next({status: 409, message: 'Category already exists'});
     }
 }
 
@@ -27,6 +49,8 @@ async function getCategories(req, res, next) {
             const categories = await Category.findAll({include: [Shirt]})
             return res.status(200).json(categories)
         } else {
+            if (!validateBody(name)) {return next({status: 400, message: 'Bad body request'})};
+            console.log()
             const categories = await Category.findAll({
                 where: {
                     name: {
@@ -45,7 +69,9 @@ async function getCategories(req, res, next) {
 
 async function deleteCategory(req, res, next) {     
     const categoryId = req.params.id
-    try { 
+    try {
+        if (isNaN(categoryId)) {return next({status: 400, message: 'Bad ID format'})}
+
         const category = await Category.findOne({where: {id: categoryId}, include: [Shirt]})
         if (category) {
             category.destroy()
@@ -61,10 +87,15 @@ async function deleteCategory(req, res, next) {
 
 async function putCategory(req, res, next) {
     const categoryId = req.params.id     
-    //body must send data to modify
-    const body = {...req.body, name: req.body.name.toLowerCase()}          //cambio el alias
-    const HEADERS = Object.keys(body)   //guardo en un array las keys del body (o sea la columnas de la tabla)
-    try {                               //buscamos el id
+    
+    try {
+        const name = validateBody(req.body)
+        
+        if (!name) {return next({status: 400, message: 'Bad body request'})};
+
+        const body = {...req.body, name: name.toLowerCase()}         
+        const HEADERS = Object.keys(body)   
+                                   
         const category = await Category.findOne({where: {id: categoryId}, include: [Shirt]}) 
         if (body.name) {
             for (const header of HEADERS) {  //tomamos cada columna 
