8e63c122f67dd71b4b8a04345473ac0245a2ff7e
HiAIDev <asahide0026@gmail.com>
2021-05-12 12:56:56 -0300

    Update app.js

--- ESHIRT/api/src/app.js
+++ ESHIRT/api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const path = rqeuire('path');
+const path = require('path');
 
 require('./db.js');
 
