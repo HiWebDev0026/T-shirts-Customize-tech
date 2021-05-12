ea4c110cd81381818fae6634ca19c04eda53d956
HiAIDev <asahide0026@gmail.com>
2021-05-12 13:07:13 -0300

    Update db.js

--- ESHIRT/api/src/db.js
+++ ESHIRT/api/src/db.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
+/*const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-} = process.env;
+} = process.env;*/
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 const basename = path.basename(__filename);
 
