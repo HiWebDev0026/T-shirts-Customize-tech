154cc7d94c8cf47719273310a6114dba07d5fe94
HiAIDev <asahide0026@gmail.com>
2021-05-12 13:51:14 -0300

    Cart Reducer fixed

--- ESHIRT/client/src/Actions/cart.js
+++ ESHIRT/client/src/Actions/cart.js
@@ -1,21 +1,12 @@
 
 
-export function getItems(){
-    
-    return (dispatch) => {
-        dispatch({type: 'GET_ITEMS'})
-    }
-}
-
 export function pushItem(item){
-    
     return (dispatch) => {
         dispatch({type: 'PUSH_ITEM', payload: item})
     }
 }
 
 export function deleteItem(id){
-    
     return (dispatch) => {
         dispatch({type: 'DELETE_ITEM', payload: id})
     }
--- ESHIRT/client/src/Components/Cart/SideCart.jsx
+++ ESHIRT/client/src/Components/Cart/SideCart.jsx
@@ -3,44 +3,51 @@ import style from './SideCart.module.css'
 import {useDispatch, useSelector} from 'react-redux'
 import {useState, useEffect} from 'react'
 import { deleteItem, pushItem } from '../../Actions/cart'
-import { emphasize } from '@material-ui/core'
+
 
 export function SideCart(){
     const [total, setTotal]= useState(0)
     const [count, setCount]= useState(0)
     const dispatch= useDispatch()
     const items= useSelector(state => state.cartReducer.items)
-    const amount= useSelector(state => state.cartReducer.amount)
+    const amount= useSelector(state => state.cartReducer.amount) // {id: cantidad, id2: cantidad}
+    const [flag, setFlag]= useState(false)
     
+    console.log(items, amount)
+
     function handleAdd(){
         dispatch(pushItem({
             name: 'Ema',
             id: 2,
-            price: 100
+            price: 100,
+            size: 'XL'
         }))
+        setFlag(!flag)
     }
 
     function handleDelete(){
         dispatch(deleteItem(2))
+        setFlag(!flag)
     }
 
 
     useEffect(() => {
         let number= Object.keys(amount)
         setCount(number.length)
-    }, [items])
+    }, [flag])
 
     return (
         <div>
             <div className={style.items}>
                 <h2>You have {count} items in your cart</h2>
-                {items?.length>0 ? items.map(item => {
+                {items => items.map(item => {
                     setTotal(total + item.price)
                     return(
                         
                         <div className={style.item}>
                             <div className={style.data}>
                                 <h4>{item.name}</h4>
+                                <h4>{item.size}</h4>
 
                                 ${item.price}x{amount[item.id]}
 
@@ -48,7 +55,7 @@ export function SideCart(){
                             <img src={item.print}/>
                         </div>
                     )
-                    }) : <div>There's 0 items in your cart</div>
+                    })
                 } 
             </div>
             <div className={style.total}>
