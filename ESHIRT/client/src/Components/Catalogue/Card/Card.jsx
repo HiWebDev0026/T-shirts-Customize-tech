24d7c7a12648dc5c10b49a77646a91f82bbf22b9
HiAIDev <asahide0026@gmail.com>
2021-05-13 12:27:07 -0300

    popup cart

--- ESHIRT/client/src/Components/Catalogue/Card/Card.jsx
+++ ESHIRT/client/src/Components/Catalogue/Card/Card.jsx
@@ -1,81 +1,93 @@
 import style from "./Card.module.css";
 import React from "react";
-import IconButton from "@material-ui/core/IconButton";
-import DehazeIcon from '@material-ui/icons/Dehaze';
-
+import {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import { FaCartPlus } from "react-icons/fa";
+import {pushItem, deleteItem, addOne, outOne, changeSize} from '../../../Actions/cart.js'
 
-function Card({title, score, size, model, color, image, id}){
+
+
+
+function Card({ title, score, size, model, color, image, id }) {
+const red = useSelector(state => state.cartReducer.items) 
+const dispatch = useDispatch()
+const [item, setItem] = useState({
+  title, score, size, model, color, image, id, amount: 1
+})
+
+function handleAdd () {
+  dispatch(pushItem(item))
+}
+
+function handleDelete () {
+  dispatch(deleteItem(item.id))
+}
+
+function handleSizeChange (e) {
+ setItem({...item, size: e.target.value})
+dispatch(changeSize(item))
+}
+
+function handleAddOne () {
   
+dispatch(addOne(item.id))
+}
+function handleOutOne () {
+dispatch(outOne(item.id))
+}
 
 
-
-
+console.log(red)
   return (
-   <div>
-    <div className={style.wrapper}>
-       
-      <div className={style.container}>
-        <div className={style.top}>
-          <img className={style.image} src= {image}/>
-        </div>
-        <div className={style.bottom}>
-          <div className={style.left}>
-            <div className={style.details}> 
-            <a >{title}</a>                     
+    <div >
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div className={style.top}>
+            <img className={style.image} src={image} />
+          </div>
+          <div className={style.bottom}>
+            <div className={style.left}>
+              <div className={style.details}>
+                <a>{title}</a>
+              </div>
             </div>
-           
-         
           </div>
         </div>
-
       </div>
-    
-  
-  
-      
-      {/* <div className={style.inside}>
-        <div className={style.icon}>
-      
-        </div>
-        <div className={style.contents}>
-          <table>
-            <tr>
-              <th>Size</th>
-              <th>Score</th>
-            </tr>
-            <tr>
-              <td>{size}</td>
-              <td>{score}</td>
-            </tr>
-            <tr>
-              <th>Model</th>
-              <th>Color</th>
-            </tr>
-            <tr>
-              <td>{model}</td>
-              <td>{color}</td>
-            </tr>
-          </table>
-        </div>
-      </div> */}
-    </div>
-        <a className={style.button} href="#popup">More info</a>
-        <div className={style.popup} id="popup">
-          <div className={style.popup_inner}>
-            <div className={style.popup__photo}>
-              
-          
-            <div className={style.popup__text}>
+      <a className={style.button} href={`#popup${id}`} >
+        More info
+      </a>
+      <div className={style.popup} id={`popup${id}`} >
+        <div className={style.popup_inner}>
+        
+          <div className={style.popup__photo}>
+            <img src={image}/>
+            
+            <a className={style.popup__close} href="#">
+              X
+            </a>
+          </div>
+            <button onClick={handleAddOne}>+ 1</button>
+            <button onClick={handleOutOne}>- 1</button>
+            <select onChange={handleSizeChange} >
+              <option>Change size</option>
+              <option value="xl">XL</option>
+              <option value="l">L</option>
+              <option value="m">M</option>
+              <option value="s">S</option>
+              </select >
+            <button onClick={handleAdd}>Add to Cart<FaCartPlus /></button>
+            <button onClick={handleDelete}>Delete</button> 
+          <div className={style.popup__text}>
               <h1>Details</h1>
               <p>{size}</p>
               <p>{title}</p>
             </div>
-            <a className={style.popup__close} href="#">X</a>
-          </div>
+            
         </div>
+       
       </div>
-      </div>
+    </div>
   );
 }
 
