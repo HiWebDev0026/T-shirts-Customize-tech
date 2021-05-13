d2592f31dc6b741cd05f611208416731f572697b
HiAIDev <asahide0026@gmail.com>
2021-05-13 18:03:23 -0300

    new detail

--- ESHIRT/client/src/Components/Catalogue/Card/Card.jsx
+++ ESHIRT/client/src/Components/Catalogue/Card/Card.jsx
@@ -1,45 +1,52 @@
 import style from "./Card.module.css";
 import React from "react";
-import {useState, useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { FaCartPlus } from "react-icons/fa";
-import {pushItem, deleteItem, addOne, outOne, changeSize} from '../../../Actions/cart.js'
+import {
+  pushItem,
+  deleteItem,
+  addOne,
+  outOne,
+  changeSize,
+} from "../../../Actions/cart.js";
 
+function Card({ title, score, size, model, color, image, id, amount }) {
+  const red = useSelector((state) => state.cartReducer.items);
+  const dispatch = useDispatch();
+  const [item, setItem] = useState({
+    title,
+    score,
+    size,
+    model,
+    color,
+    image,
+    id,
+    amount: 1,
+  });
 
+  function handleAdd() {
+    dispatch(pushItem(item));
+  }
 
+  function handleDelete() {
+    dispatch(deleteItem(item.id));
+  }
 
-function Card({ title, score, size, model, color, image, id }) {
-const red = useSelector(state => state.cartReducer.items) 
-const dispatch = useDispatch()
-const [item, setItem] = useState({
-  title, score, size, model, color, image, id, amount: 1
-})
+  function handleSizeChange(e) {
+    setItem([...item, { size: e.target.value }]);
+    dispatch(changeSize(item));
+  }
 
-function handleAdd () {
-  dispatch(pushItem(item))
-}
+  function handleAddOne() {
+    dispatch(addOne(item.id));
+  }
+  function handleOutOne() {
+    dispatch(outOne(item.id));
+  }
 
-function handleDelete () {
-  dispatch(deleteItem(item.id))
-}
-
-function handleSizeChange (e) {
- setItem({...item, size: e.target.value})
-dispatch(changeSize(item))
-}
-
-function handleAddOne () {
-  
-dispatch(addOne(item.id))
-}
-function handleOutOne () {
-dispatch(outOne(item.id))
-}
-
-
-console.log(red)
   return (
-    <div >
+    <div>
       <div className={style.wrapper}>
         <div className={style.container}>
           <div className={style.top}>
@@ -54,38 +61,50 @@ console.log(red)
           </div>
         </div>
       </div>
-      <a className={style.button} href={`#popup${id}`} >
+      <a className={style.button} href={`#popup${id}`}>
         More info
       </a>
-      <div className={style.popup} id={`popup${id}`} >
+      <div className={style.popup} id={`popup${id}`}>
         <div className={style.popup_inner}>
-        
           <div className={style.popup__photo}>
-            <img src={image}/>
-            
+            <img src={image} />
+
             <a className={style.popup__close} href="#">
               X
             </a>
           </div>
-            <button onClick={handleAddOne}>+ 1</button>
-            <button onClick={handleOutOne}>- 1</button>
-            <select onChange={handleSizeChange} >
-              <option>Change size</option>
-              <option value="xl">XL</option>
-              <option value="l">L</option>
-              <option value="m">M</option>
-              <option value="s">S</option>
-              </select >
-            <button onClick={handleAdd}>Add to Cart<FaCartPlus /></button>
-            <button onClick={handleDelete}>Delete</button> 
+
           <div className={style.popup__text}>
-              <h1>Details</h1>
-              <p>{size}</p>
-              <p>{title}</p>
+            <h1>Details</h1>
+            <h2>{title}</h2>
+            <p>Size: {size}</p>            
+            <p>Color: {color}</p>
+            <p>Model: {model}</p>
+            <p>Score: {score}</p>
+            <p>Amount: {amount}</p>
+            <div className={style.cartBox}>
+              <button className={style.buttonCart} onClick={handleAddOne}>
+                + 1
+              </button>
+              <button className={style.buttonCart} onClick={handleOutOne}>
+                - 1
+              </button>
+              <select className={style.buttonCart} onChange={handleSizeChange}>
+                <option>Change size</option>
+                <option value="xl">XL</option>
+                <option value="l">L</option>
+                <option value="m">M</option>
+                <option value="s">S</option>
+              </select>
+              <button className={style.buttonCart} onClick={handleAdd}>
+                <FaCartPlus />
+              </button>
+              <button className={style.buttonCart} onClick={handleDelete}>
+                Delete
+              </button>
             </div>
-            
+          </div>
         </div>
-       
       </div>
     </div>
   );
