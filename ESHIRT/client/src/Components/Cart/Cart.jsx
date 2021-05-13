52e49d70a7de1600e9e4d0a92078879ef98de70a
HiAIDev <asahide0026@gmail.com>
2021-05-13 18:31:36 -0500

    14 version

--- ESHIRT/client/src/Components/Cart/Cart.jsx
+++ ESHIRT/client/src/Components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React,{useEffect,useState} from 'react';
-// import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+// import {} from '../../Actions/cart.js'
+import {Link} from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
-import { BsFillHeartFill,BsFillTrashFill } from 'react-icons/bs';
-import { FaEdit } from "react-icons/fa";
 
+
+import CartItem from './CartItem.jsx'
 import Style from './Cart.module.css'
 
 export default function Cart (){
@@ -28,15 +30,16 @@ export default function Cart (){
             size: 'S',
             id: 12323,
             price: 30,
-            quantity:1
-        },
+            amount:1
+        }
+        ,
         {
             name: 'Ema',
             print: 'https://http2.mlstatic.com/D_NQ_NP_634948-MLA32874228192_112019-W.jpg',
             size: 'XL',
             id: 87879,
             price: 35,
-            quantity:2
+            amount:2
         },
         {
             name: 'Ger',
@@ -44,7 +47,7 @@ export default function Cart (){
             size: 'L',
             id: 263625365,
             price: 40,
-            quantity:3
+            amount:3
         },
         {
             name: 'Eze',
@@ -52,7 +55,7 @@ export default function Cart (){
             size: 'M',
             id: 895868975,
             price: 30,
-            quantity:1
+            amount:1
         },
         {
             name: 'Javi',
@@ -60,7 +63,7 @@ export default function Cart (){
             size: 'M',
             id: 8787887709,
             price: 35,
-            quantity:2
+            amount:2
         },
         {
             name: 'Lean',
@@ -68,7 +71,7 @@ export default function Cart (){
             size: 'M',
             id: 7978878,
             price: 40,
-            quantity:3
+            amount:3
         },
         {
             name: 'Agus',
@@ -76,59 +79,26 @@ export default function Cart (){
             size: 'M',
             id: 66666655,
             price: 40,
-            quantity:3
-        }]
+            amount:3
+        }
+    ]
     
     const [products,setProducts]=useState([]);
-    const [remove,setRemove]=useState(false);
-    // const [edit,setEdit]=useState(false);
     const [currentPage, setCurrentPage] = useState(0);
-    // const [input,setInput] =useState(it.quantity)
 
-    let sizes=['S','M','L','XL']    
+    const  dispatch= useDispatch();
+    // const items = useSelector((state)=>state.cartReducer.items);
+    
     const INITIAL_PAGE= 4;
     const offset = currentPage * INITIAL_PAGE;
     const pageCount = Math.ceil(products.length / INITIAL_PAGE);
 
-   
-
     useEffect(()=>{
-        setProducts(items);
-        console.log('PRODUCTS',products);
-    },[]);
-
-   
-
-
-    function handleDelete(e){
-        let id=e.currentTarget.value;
-        console.log('ID',id);
-        const cartItems= products.slice();
-        // setState({cartItems:cartItems.filter(i=>i.d !==item.id)});
-        //setStatus es el global y cartItems: es para un estado global que es cartitems: [];
-        //lo que viene es mientras tanto
-        console.log('CARTITEMS',cartItems);
-        let copy= cartItems.filter(item=>item.id != id);
-        console.log('COPY',copy);
+        let copy= items.slice();
         setProducts(copy);
-        setRemove(!remove);
-    }
-
-    // function handleEdit(e) {
-    //     let id=e.currentTarget.value;
-    //     console.log('IDEDIT',id);
-    //     const cartItems= products.slice();
-    //     let index= cartItems.findIndex(item=>item.id == id);
-    //     console.log('Index',index);
-    //     products[index].size='L';
-    //     console.log('SIZE',products[index].size);
-    //     setEdit(!edit);
-    // }
-
-    // function onChangeHandler(e){
-    //     setInput(e.target.value);
-
-    // }
+        console.log('COPY',copy);
+        console.log('PROD',products);
+    },[]);
 
     function handlePageClick({ selected: selectedPage }) {
         setCurrentPage(selectedPage);
@@ -145,44 +115,7 @@ export default function Cart (){
                         {
                             products.length>0?
                             products.slice(offset, offset + INITIAL_PAGE).map(it=>{
-                                return <li key={it.id} className={Style.cartCard}> 
-                                            <div className={Style.picture}>
-                                                <img src={it.print} alt={'product'+ it.id} className={Style.image}/>
-                                            </div>
-                                            <div className={Style.column1}>
-                                                <div className={Style.detail}>
-                                                    <div className={Style.name}>{it.name}</div>
-                                                    <div className={Style.sku}>SKU:{it.id}</div>
-                                                </div>
-                                                <div className={Style.btns}>
-                                                        <button value={it.id} onClick={handleDelete}><BsFillTrashFill/></button>
-                                                        <button><BsFillHeartFill/></button>
-                                                        {/* <Link to='/design'> */}
-                                                            <button><FaEdit/></button>
-                                                        {/* </Link> */}
-                                                 
-                                                </div>
-                                            </div>
-                                            <div className={Style.column2}>
-                                                <div className={Style.price}>${it.price}</div>
-                                            </div>
-                                            <div className={Style.column3}>
-                                                <div className={Style.size}>
-                                                    <select name='size' id='size' required>
-                                                        {
-                                                            sizes.map((s)=>{
-                                                                return it.size === s?
-                                                                  <option value={s} selected>{s}</option>
-                                                                : <option value={s}>{s}</option>
-                                                            })  
-                                                        }
-                                                    </select>
-                                                </div>
-                                                <div className={Style.quantity}>
-                                                    <input type="number"  min="1" max="10"  className={Style.q}/>
-                                                </div>
-                                            </div>
-                                        </li>
+                                return <CartItem  it={it} key={it.id}  className={Style.cartCard}/>      
                             })
                         :<p>No selected items</p>
                         }
@@ -197,9 +130,11 @@ export default function Cart (){
                         <div>Your cart is empty</div>
                         :<div>You have {products.length} items in your shopping cart</div>
                     }</div>
-                    <div>${products.reduce((a,c)=>a+c.price*c.quantity,0)}</div>
-                    <button>Go back shopping</button>
-                    <button>Purchase</button>
+                    <div>${products.reduce((a,c)=>a+c.price*c.amount,0)}</div>
+                    <Link to='/catalogue'>
+                        <button>Go back shopping</button>
+                    </Link>
+                    {products.length >0&&<button>Purchase</button>}
                     
                 </div>
             </div>
