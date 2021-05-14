c2258093add0d08f21190ed1439681123f6ca7af
HiAIDev <asahide0026@gmail.com>
2021-05-14 02:50:02 -0300

    excel for shirts and table admin

--- ESHIRT/client/src/Components/Admin/ShirtsAdmin/ShirtsAdmin.jsx
+++ ESHIRT/client/src/Components/Admin/ShirtsAdmin/ShirtsAdmin.jsx
@@ -20,53 +20,47 @@ const [max, setMax] = useState(0);
         alert("Shirt " + e.target.value + " deleted");
         dispatch(deleteShirt(parseInt(e.target.value))); 
       };
-      function handleScroll() {
- 
-        let image;
-        let nextImg ;
-        image = Math.floor(Math.random()*1000)
-        nextImg = image +1
-      }
-      useEffect(() => {setMax(shirts.length - 17); setPage(0);}, [shirts]);
-      const nextPage = () => { page < max && setPage(page + 17); };
-      const prevPage = () => { page > 0 && setPage(page - 17); };
+      
+      useEffect(() => {setMax(shirts.length - 11); setPage(0);}, [shirts]);
+      const nextPage = () => { page < max && setPage(page + 11); };
+      const prevPage = () => { page > 0 && setPage(page - 11); };
     
     return(
       <div>
-
         <div className={Style.General} >
         <table id="table-to-xls">
         <div className={Style.Shirts} id='tableShirts'>
-            
+            <br/>
             <tr>
-             <th className={Style.Title1}> Id-</th>
-              <th className={Style.Title2}> -Name-</th>
-              <th className={Style.Title3}> -Color-</th>
-              <th className={Style.Title4}> -Model-</th>
-              <th className={Style.Title5}>-Size-</th>
-              <th className={Style.Title6}> -Score-</th>
-              <th className={Style.Title7}> -Public-</th>
-              <th className={Style.Title8}> -Created-</th>
-              <th className={Style.Btn}>-Delete</th>
+             <th className={Style.Title1}> Id---------------</th>
+              <th className={Style.Title2}> -------------Name-------------</th>
+              <th className={Style.Title3}> -------------Color-------------</th>
+              <th className={Style.Title4}> -------------Model--------------</th>
+              <th className={Style.Title5}>-------------Size-------------</th>
+              <th className={Style.Title6}> -------------Score-------------</th>
+              <th className={Style.Title7}> -------------Public-------------</th>
+              <th className={Style.Title8}> -------------Created-------------</th>
               </tr>
               </div>
               
             {shirts.length > 0 
-      ? ( shirts.slice(page, page + 17).map((shirt) => {
+      ? ( shirts.slice(page, page + 11).map((shirt) => {
           return (
+            <tr>
               <div className={Style.Tarjet} >
-                <tr>
+                
               <th className={Style.Titles1}> {shirt.id}</th>
-              <th className={Style.Titles2}> -{shirt.name}-</th>
-              <th className={Style.Titles3}> -{shirt.color}-</th>
-              <th className={Style.Titles4}> -{shirt.model}-</th>
-              <th className={Style.Titles5}> -{shirt.size}-</th>
-              <th className={Style.Titles6}> -{shirt.score}-</th>
-              <th className={Style.Titles7}> -{shirt.public}-</th>
-              <th className={Style.Titles8}> -{shirt.created_by_user}-</th>
+              <th className={Style.Titles2}> {shirt.name}</th>
+              <th className={Style.Titles3}> {shirt.color}</th>
+              <th className={Style.Titles4}> {shirt.model}</th>
+              <th className={Style.Titles5}> {shirt.size}</th>
+              <th className={Style.Titles6}> {shirt.score}</th>
+              <th className={Style.Titles7}> {shirt.public}</th>
+              <th className={Style.Titles8}> {shirt.created_by_user}</th>
               <th><button className={Style.Btn1} value={shirt.id} onClick={handleDelete}>X</button></th>
-              </tr>
+             
               </div>
+               </tr>
           );
         })
       ) 
