efd7a6c58bb2d89488dad02f95b1a074d6065892
HiAIDev <asahide0026@gmail.com>
2021-05-14 12:34:41 -0300

    backend middleware authz folder

--- ESHIRT/api/src/routes/shirt.js
+++ ESHIRT/api/src/routes/shirt.js
@@ -1,31 +1,14 @@
 const {Router}= require('express')
 const {postShirt, getShirts, getShirt, putShirt, deleteShirt}= require('../controllers/shirt')
-const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
-const jwksRsa = require('jwks-rsa');
+const checkJwt = require('../middlewares/authz/checkJwt');
 const router= Router();
 
-const checkJwt = jwt({
-    // Dynamically provide a signing key
-    // based on the kid in the header and 
-    // the signing keys provided by the JWKS endpoint.
-    secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
-    }),
-  
-    // Validate the audience and the issuer.
-    audience:  `${process.env.REACT_APP_AUTH0_AUDIENCE}`,
-    issuer: [`${process.env.REACT_APP_AUTH0_DOMAIN}`],
-    algorithms: ['RS256']
-  });
+
 
 router.get('/', getShirts)
 router.get('/:id', getShirt)
 router.post('/', checkJwt, postShirt)
-router.put('/:id', putShirt)
-router.delete('/:id', deleteShirt)
+router.put('/:id', checkJwt, putShirt)
+router.delete('/:id', checkJwt, deleteShirt)
 
 module.exports = router
