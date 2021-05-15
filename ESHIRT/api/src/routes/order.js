c462f6aa1d983f9341300908f4bdddd12abc0648
HiAIDev <asahide0026@gmail.com>
2021-05-15 20:42:49 -0300

    order get methods implemented on back and front

--- ESHIRT/api/src/routes/order.js
+++ ESHIRT/api/src/routes/order.js
@@ -1,11 +1,12 @@
-const {Router}= require('express')
-const {postOrder, getOrder, getOrders, putOrder, modifyStatus}= require('../controllers/order')
-const router= Router()
+const {Router} = require('express')
+const {postOrder, getOrder, getOrders, putOrder, modifyStatus, getOrdersByUserId}= require('../controllers/order')
+const router = Router()
 
 router.get('/', getOrders)
 router.get('/:id', getOrder)
 router.post('/:userId', postOrder)
 router.put('/:id', putOrder)
 router.put('/status/:id', modifyStatus)
+router.get('/user/:userId', getOrdersByUserId)
 
-module.exports= router
+module.exports = router
