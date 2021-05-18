0a9f1b10ef4a46af960142f01e7126f18665c10a
HiAIDev <asahide0026@gmail.com>
2021-05-18 13:10:49 -0300

    fix routing

--- ESHIRT/client/src/Components/Admin/RecycleBin/RecycleBin.jsx
+++ ESHIRT/client/src/Components/Admin/RecycleBin/RecycleBin.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import Style from './RecycleBin.module.css'
+import {useTokenDecode} from '../../../hooks/tokenDecoding';
+import ErrorNoAdminPage from '../ErrorPages/ErrorNoAdmin';
 
 
 
 function RecycleBin() {
 
+    const isAdmin = useTokenDecode(localStorage.currentToken)
 
     return (
+        !isAdmin ? (<ErrorNoAdminPage />) :
         <div className={Style.container}>
             <h1>I'm trash, and yet, so useful</h1>
         </div>
