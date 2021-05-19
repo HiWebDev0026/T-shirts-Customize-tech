5ab8161501f5f2cbb605e2f86ff91a1979cbdcb3
HiAIDev <asahide0026@gmail.com>
2021-05-19 15:05:38 -0300

    css reviews

--- ESHIRT/client/src/Actions/shirt.js
+++ ESHIRT/client/src/Actions/shirt.js
@@ -100,17 +100,10 @@ export function getShirtReview(shirtId){
     return async (dispatch) => {
         try {
             const res = await axios.get(`/shirt/${shirtId}/review`, {responseType: 'json'})
-            const shirt = res.data
-
-         
-
-
-            console.log(shirt)
-
+            const shirt = res.data      
+           console.log(shirt)
             dispatch({type: 'GET_SHIRT_REVIEW', payload: shirt})
-
            
-
         } catch (err) {
             console.log((err.response && err.response.data) || 'Server not working!');
             dispatch({type: 'HANDLE_REQUEST_ERROR', payload: (err.response && err.response.data) || {status: 500, message: 'Server problem'}})
