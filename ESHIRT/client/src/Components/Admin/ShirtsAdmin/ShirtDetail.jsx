b8a73b8275cb96f6bf00bea7c3ad1fdde4380170
HiAIDev <asahide0026@gmail.com>
2021-05-20 21:47:20 -0300

    admin now can edit the shirts

--- ESHIRT/client/src/Components/Admin/ShirtsAdmin/ShirtDetail.jsx
+++ ESHIRT/client/src/Components/Admin/ShirtsAdmin/ShirtDetail.jsx
@@ -13,10 +13,37 @@ export default function ShirtDetail() {
 const shirt = useSelector((state) => state.shirtReducer.shirtId);
 const history = useHistory();
 const dispatch = useDispatch();
-const [page, setPage] = useState(0);
-const [max, setMax] = useState(0);
 const [count, setCount] = useState([]);
 const isAdmin = useTokenDecode(localStorage.currentToken);
+
+const ID = parseInt(shirt.id)
+
+const [input, setInput] = useState({
+    name: '',
+    color: '',
+    model: '',
+    size: '',
+});
+
+useEffect(() => {
+    dispatch(getShirtById());
+}, []);
+
+function handleChange(e) {
+    const value = e.target.value;
+    const name = e.target.name
+
+    setInput({
+        ...input,
+        [name]: value
+    });
+}
+
+function handleEdit(e) {
+dispatch(putShirt({...input}, e.target.value)); 
+history.push('/shirts_admin')
+}
+console.log(shirt)
 
 return(
     !isAdmin ? (<ErrorNoAdminPage />) : <div>
@@ -30,8 +57,19 @@ return(
               <th className={Style.Titles6}> {shirt.score}</th>
               <th className={Style.Titles7}> {shirt.public}</th>
               <th className={Style.Titles8}> {shirt.created_by_user}</th>
+              <img src={shirt.print} className={Style.Image}/>
               </div>
-
+              <div>
+              <h5>Do you want to change the shirt?</h5>
+                 <input name = 'name' className= 'name' type = 'text' placeholder= 'Name:' onChange= {handleChange} required/>
+                 <input name = 'color' className= 'color' type = 'text' placeholder= 'Color:' onChange= {handleChange} required/>
+                 <input name = 'model' className= 'model' type = 'text' placeholder= 'Model:' onChange= {handleChange} required/>
+                 <input name = 'size' className= 'size' type = 'text' placeholder= 'Size:' onChange= {handleChange} required/>
+                 
+                 <button value={shirt.id} type='submit' onClick={handleEdit}>Change</button>      
+                           
+        
+                    </div>
 <NavLink to='shirts_admin'>
     <h4 className={Style.Btn3}>SHIRTS</h4>
     </NavLink>  
