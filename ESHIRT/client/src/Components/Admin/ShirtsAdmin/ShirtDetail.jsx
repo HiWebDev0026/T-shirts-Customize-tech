cf36fbc1da99be31d55c42c7fa6dfe9d6aab84e8
HiAIDev <asahide0026@gmail.com>
2021-05-21 01:20:21 -0300

    admin can add categories for shirts and modifies it

--- ESHIRT/client/src/Components/Admin/ShirtsAdmin/ShirtDetail.jsx
+++ ESHIRT/client/src/Components/Admin/ShirtsAdmin/ShirtDetail.jsx
@@ -2,31 +2,33 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {NavLink} from 'react-router-dom';
 import { getShirts, deleteShirt, putShirt, getShirtById } from "../../../Actions";
+import {getCategories} from '../../../Actions'
 import ReactHTMLTableToExcel from 'react-html-table-to-excel';
-import Style from "./ShirtsAdmin.module.css";
+import Style from "./ShirtDetail.module.css";
 import {useTokenDecode} from '../../../hooks/tokenDecoding';
 import ErrorNoAdminPage from '../ErrorPages/ErrorNoAdmin';
 import {useHistory} from 'react-router-dom';
 
 export default function ShirtDetail() {
-
+const categories = useSelector((state)=> state.categoryReducer.allCategories)
 const shirt = useSelector((state) => state.shirtReducer.shirtId);
 const history = useHistory();
 const dispatch = useDispatch();
 const [count, setCount] = useState([]);
+const [temp, SetTemp] = useState([]);
 const isAdmin = useTokenDecode(localStorage.currentToken);
-
-const ID = parseInt(shirt.id)
 
 const [input, setInput] = useState({
     name: '',
     color: '',
     model: '',
     size: '',
+   
 });
 
 useEffect(() => {
     dispatch(getShirtById());
+    dispatch(getCategories());
 }, []);
 
 function handleChange(e) {
@@ -39,15 +41,19 @@ function handleChange(e) {
     });
 }
 
+let array= []
+function handleChange1(e) {
+     array.push([parseInt(e.target.value)])
+}
+
 function handleEdit(e) {
-dispatch(putShirt({...input}, e.target.value)); 
+    alert('Shirt modified')
+dispatch(putShirt({...input, categories: array}, e.target.value)); 
 history.push('/shirts_admin')
 }
-console.log(shirt)
 
 return(
-    !isAdmin ? (<ErrorNoAdminPage />) : <div>
-
+    !isAdmin ? (<ErrorNoAdminPage />) : <div className={Style.General}>
 <div className={Style.Tarjet} >
               <th className={Style.Titles1}> {shirt.id}</th>
               <th className={Style.Titles2}> {shirt.name}</th>
@@ -59,16 +65,21 @@ return(
               <th className={Style.Titles8}> {shirt.created_by_user}</th>
               <img src={shirt.print} className={Style.Image}/>
               </div>
-              <div>
-              <h5>Do you want to change the shirt?</h5>
+              <div className={Style.Changes}>
+              <h5 className={Style.ChangesTitle}>Do you want to change it?</h5>
                  <input name = 'name' className= 'name' type = 'text' placeholder= 'Name:' onChange= {handleChange} required/>
                  <input name = 'color' className= 'color' type = 'text' placeholder= 'Color:' onChange= {handleChange} required/>
                  <input name = 'model' className= 'model' type = 'text' placeholder= 'Model:' onChange= {handleChange} required/>
                  <input name = 'size' className= 'size' type = 'text' placeholder= 'Size:' onChange= {handleChange} required/>
+                 <div className={Style.Categories}>
+                        <label className={Style.ChangesTitle} for="categories">Chose the categories of the shirt: </label>
+                        <select className={Style.Categories1} onChange={handleChange1} name="categories">
+                            {categories.map((elem, index) => (<option className={Style.Categories1} value={elem.id} key={index}>{elem.name}</option>))}
+                        </select>
+                    </div>
                  
-                 <button value={shirt.id} type='submit' onClick={handleEdit}>Change</button>      
+                 <button className={Style.BtnChange} value={shirt.id} type='submit' onClick={handleEdit}>Change</button>      
                            
-        
                     </div>
 <NavLink to='shirts_admin'>
     <h4 className={Style.Btn3}>SHIRTS</h4>
