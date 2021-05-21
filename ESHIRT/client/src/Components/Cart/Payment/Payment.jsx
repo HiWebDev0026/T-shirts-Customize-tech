1dfffa9f9c341ea1645b94563fbff3c681707f62
HiAIDev <asahide0026@gmail.com>
2021-05-20 21:00:57 -0300

    Payment update

--- ESHIRT/client/src/Components/Cart/Payment/Payment.jsx
+++ ESHIRT/client/src/Components/Cart/Payment/Payment.jsx
@@ -10,7 +10,7 @@ function Payment() {
     const paymentData = useSelector((state)=>state.paymentReducer.paymentData)
     const items = useSelector((state)=>state.cartReducer.items);
     const dispatch= useDispatch()
-    const {isAuthenticated, loginWithPopup}= useAuth0()
+    const {isAuthenticated}= useAuth0()
     
     const [deliveryData, setDeliveryData]= useState({
         zip_code: '',
@@ -60,15 +60,13 @@ function Payment() {
             console.log(order, shipments)
             dispatch(createPayment(order, shipments))
             setFlag(true)
-            window.open(paymentData?.response?.init_point)
-
         } 
     }
 
-//     /* Lo de adentro del return va en shipments.receiver_address */
-
     return (
+        <div className={style.container}>
         <form onSubmit={handleSubmit} className={style.form}>
+            <div>Please, complete your address:</div>
             <input placeholder= 'Zip code' id='zip_code' onChange={handleChange} />
             <input placeholder= 'Street name' id='street_name' onChange={handleChange} />
             <input placeholder= 'Street number' id='street_number' onChange={handleChange} type='number' />
@@ -78,11 +76,17 @@ function Payment() {
             <input placeholder= 'State' id='state_name' onChange={handleChange} />
             <input placeholder= 'Country' id='country_name' onChange={handleChange} />
             {
-            items.length >0 && <button type='submit'>Pay!</button>
+                flag ? 
+            <a className={(paymentData?.response?.init_point && style.mercadopago) || style.inactive} target='_blank' href={paymentData?.response?.init_point} rel='nofollow'>
+                
+            </a>  
+                :
+            items.length >0 && <button type='submit'>All set!</button>
             }
             
         </form>
+        </div>
     )
 }
 
-// export default Payment
+export default Payment
