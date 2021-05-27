b3ad48e9893149d0689316df4b5e4beaeb39f90c
HiAIDev <asahide0026@gmail.com>
2021-05-27 01:11:57 -0500

    landing page

--- ESHIRT/client/src/Components/Landing/Landing.jsx
+++ ESHIRT/client/src/Components/Landing/Landing.jsx
@@ -1,43 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import Style from './Landing.module.css';
-import {NavLink} from 'react-router-dom';
-import A from '../../Images/1.png';
-import B from '../../Images/2.png';
-import C from '../../Images/3.png';
-import D from '../../Images/4.png';
-import E from '../../Images/5.png';
-import F from '../../Images/6.png';
-import G from '../../Images/7.png';
-import H from '../../Images/8.png';
-import I from '../../Images/9.png';
-
-
-
-
-
 
 
 function Landing() {
     
-    let arrayImg = [A, B, C, D, E, F, G, H, I];
-    
-    const [image, setImage] = useState();
-    
-    useEffect(() => {
-        let buscar = 0
-        const interval = setInterval(() => {
-            setImage(arrayImg[buscar % arrayImg.length])
-            buscar++
-    }, 1000);
-        return () => clearInterval(interval);
-    }, []);
-
     return (
-        <div className={Style.container}>
-            <h1 >Welcome to E-shirt</h1>
-            <h3 className={Style.subTitle}>The place where you can desing your next shirt</h3>
-            <NavLink to='./home'><button className={Style.btn}>Come on in!</button></NavLink>
-            <img className={Style.img} src={image}/>
+        <div className={Style.general}>
+           <div className={Style.picture1}>
+               <div className={Style.section1}>
+                   E-SHIRT
+               </div>
+               <div className={Style.section2}>
+                Turn ordinary into extraordinary 
+               </div>
+           </div>
+            <section className={Style.sectionOne}>
+            <h1>SECTION1</h1>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2qbr_yUMbQs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <div className={Style.picture2}>
+               <div className={Style.section3}>
+               top-rated
+               </div>
+           </div>
+           <section className={Style.sectionTwo}>
+            <h2>SECTION2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi et porro ullam atque ut rerum dolores fuga pariatur natus aut eum, odit autem, non deleniti quos laboriosam minima iusto est! Veritatis, non ex reiciendis nemo id impedit ad eius debitis quam libero aperiam, vero velit repellendus quos! Facere, accusamus nostrum.</p>
+            </section>
+            <div className={Style.picture3}></div>
+            <section className={Style.sectionThree}>
+            <h3>SECTION3</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti eveniet sapiente placeat aperiam accusantium veritatis illo velit, enim maiores cum assumenda soluta dolor illum est reiciendis quos quam mollitia architecto. Facere ratione doloribus nesciunt facilis maxime sapiente odio debitis! Mollitia quae provident ipsa alias corporis iure debitis voluptatem ad enim.s</p>
+            </section>
+            <div className={Style.picture4}></div>
         </div>
         )
 }
