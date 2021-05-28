e0269a3126f42e462d8526c7952468291d6892ed
HiAIDev <asahide0026@gmail.com>
2021-05-27 22:23:46 -0300

    discounts

--- ESHIRT/client/src/Components/Admin/Discount/Discounts.jsx
+++ ESHIRT/client/src/Components/Admin/Discount/Discounts.jsx
@@ -15,7 +15,7 @@ export default function Discounts() {
     const categories= useSelector((state)=>state.categoryReducer.allCategories);
     const dispatch= useDispatch();
 
-    let category= ['Categories']
+    let category= ['Category']
     categories.map((temp) => {
         return category.push(temp.name)
       })
@@ -32,6 +32,7 @@ export default function Discounts() {
         console.log(value)
     }
     
+    let days= ['Day','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
     return(
         isAdmin === null ? 'LOADING' : isAdmin === false ? (<ErrorNoAdminPage />) : 
@@ -40,6 +41,11 @@ export default function Discounts() {
             <div className={Style.Title}>
                 <h1>DISCOUNTS</h1>
           
+            <select onChange={handleChange1}>
+          {days.map((temp) => {
+            return <option value={temp}>{temp} </option>; //Template
+          })}
+        </select>
 
         <select onChange={handleChange1}>
           {category.map((temp) => {
@@ -47,7 +53,7 @@ export default function Discounts() {
           })}
         </select>
         <p>Percentage</p>
-        <input type= 'number' min='0' max='75' placeholder='Choose the %' />
+        <input type= 'number' min='0' max='75' placeholder='Choose the %' onChange={handleChange1}/>
         <button>APLY</button>
 
 
