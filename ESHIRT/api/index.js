3368d849774d288838851f219c46a6eaddae3a20
HiAIDev <asahide0026@gmail.com>
2021-06-10 20:40:46 -0300

    Update index.js

--- ESHIRT/api/index.js
+++ ESHIRT/api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const {fillDB} = require('./src/fill_db.js');
 
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
     fillDB();
     server.listen(process.env.PORT, () => {
     console.log('%s listening at', process.env.PORT, '\nDATABASE UP'); // eslint-disable-line no-console
